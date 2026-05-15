491b99792ef13c7ce93732194f53a6ca576f258a
salinikqi <salinikqii4@gmail.com>
2026-05-15 16:17:31 +0200

    Add files via upload

---
+++ art.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//  $('#discover').click(function(){
+//    $('#extra').slideToggle();
+//  });
+// });
+
+$(document).ready(function(){
+ $('#discover').click(function(){
+   $('#extra').slideToggle();
+ });
+});
